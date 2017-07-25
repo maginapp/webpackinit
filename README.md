458cd4bac4bf26df899370f6ddcb069de0da7b27
maginacp <1075433743@qq.com>
2017-07-25 17:00:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#webpack
+##learn from [webpack入门指导](https://segmentfault.com/a/1190000006178770)
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by beijingweb on 2017/7/25.
+ */
+module.exports = {
+  //devtool: "source-map ",
+  devtool: "eval-source-map ", //cheap-module-source-map //source-map //配置生成Source Maps，选择合适的选项
+  entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+  output: {
+    path: __dirname + "/public",//打包后的文件存放的地方
+    filename: "bundle.js"//打包后输出文件的文件名
+  },
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  }
+}
+//注：“__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
